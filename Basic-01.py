44f364bc199d6eba29be059a825f29d294e2808d
BHANU-CHOURASIA <chourasiyabhanu327@gmail.com>
2026-09-19 18:46:27 +0530

    added basic variables code

---
+++ Basic-01.py
@@ -0,0 +1,15 @@
+# 1. Variables & Data Types
+student_name = "Bhanu"      # String (Text)
+age = 21                    # Integer (Whole Number)
+mca_marks = 82.5            # Float (Decimal)
+is_enrolled = True          # Boolean (True/False)
+
+# 2. Output Print Karna
+print("--- Student Profile ---")
+print("Name:", student_name)
+print("Age:", age)
+print("Marks:", mca_marks)
+print("Is Student Enrolled?:", is_enrolled)
+
+# 3. Dynamic Message (f-string)
+print(f"Hello, {student_name}! Welcome to Python Practice.")
